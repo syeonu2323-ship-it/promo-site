cd6665ede070b78f5f7b6d7e124d95046528d43b
여누 <yeo_nu@yeonuui-MacBookAir.local>
2025-11-26 16:17:59 +0900

    update: add ProductPhotos section

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@
 
 import Hero from "./components/Hero";
 import Problem from "./components/Problem";
+import ProductPhotos from "./components/ProductPhotos";
 
 export default function Home() {
   return (
     <main className="w-full">
       <Hero />
+      <ProductPhotos />  
       <Problem />
     </main>
   );
